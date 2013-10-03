fe25c863004f984e216bfe37cfc1a92a0b826f1c
bw4sz <benweinstein2010@gmail.com>
2013-10-03 12:05:08 -0400

    Input Data Added

---
+++ AlphaBetaMapping.R
@@ -0,0 +1,227 @@
+##Alpha Mapping
+#Ben Weinstein Stony Brook University
+
+##Goals: To develop parralell computing methods to turn niche models into assemblages and perform phylogenetic and functional betadiversity metrics
+#October 16th 2012 - Ben Weinstein. Stony Brook University
+
+#require packages
+require(RColorBrewer)
+require(biomod2)
+require(maptools)
+require(ggplot2)
+require(reshape)
+require(raster)
+require(rgdal)
+require(doSNOW)
+require(ape)
+require(stringr)
+require(FD)
+require(picante)
+require(parallel)
+require(ecodist)
+require(vegan)
+require(MASS)
+#load workspace if needed
+load("C:\\Users\\Ben\\Dropbox\\Lab paper 1 Predicted vs observed assemblages\\AlphaMapping.rData")
+
+#Bring in Phylogenetic Data
+trx<-read.nexus("C:\\Users\\Ben\\Dropbox\\Shared Ben and Catherine\\DimDivEntire\\\\Files for Analysis\\ColombiaPhylogenyUM.tre")
+spnames<-read.table(file="C:\\Users\\Ben\\Dropbox\\Shared Ben and Catherine\\DimDivEntire\\Files for Analysis\\SpNameTree.txt" , sep = "\t", header = TRUE)
+
+#Replace tip.label with Spnames#
+#replace the tiplabels with periods, which is the biomod default
+trx$tip.label<-gsub("_",".",as.character(spnames$SpName))
+co<-cophenetic(trx)
+
+#Bring in functional tree
+tree.func<-read.tree("C:\\Users\\Ben\\Dropbox\\Shared Ben and Catherine\\DimDivEntire\\Files for Analysis\\func.tre")
+tree.func$tip.label<-gsub("_",".",tree.func$tip.label)
+fco<-cophenetic(tree.func)
+
+#bring in traits
+morph <- read.csv("C:\\Users\\Ben\\Dropbox\\Lab paper 1 Predicted vs observed assemblages\\MorphologyShort.csv",na.strings="9999")
+
+#just get males
+morph.male<-morph[morph$Sex=="Macho",c("SpID","ExpC","Peso","AlCdo")]
+morph.complete<-morph.male[complete.cases(morph.male),]
+
+#aggregate for species
+agg.morph<-aggregate(morph.complete,list(morph.complete$SpID),mean)
+mon<-agg.morph[,-2]
+colnames(mon)<-c("Species","Bill","Mass","Wing Chord")
+
+#source in all the Alpha Mapping functions
+source("C:\\Users\\Ben\\Dropbox\\Scripts/Dimensions/AlphaMappingFunctions.R")
+
+####Bring in niche models
+  setwd("D:/Niche_Models/")
+
+##############################
+#Bring in Model Data
+##############################
+
+#get all the niche model data
+niche<-list.files(getwd(),pattern="TotalConsensus_EMbyROC.gri",full.name=T,recursive=T)
+
+#split into current and future
+#Get current models
+current_niche<-niche[grep("current",niche,value=FALSE)]
+
+#Get future models, for now its just
+MICROC2070rcp26_niche<-niche[grep("MICROC2070rcp26",niche,value=FALSE)]
+MICROC2070rcp85_niche<-niche[grep("MICROC2070rcp85",niche,value=FALSE)]
+MICROC2070rcp45_niche<-niche[grep("MICROC2070rcp45",niche,value=FALSE)]
+
+#create list of input rasters
+input.niche<-list(current_niche,MICROC2070rcp26_niche,MICROC2070rcp45_niche,MICROC2070rcp85_niche)
+
+names(input.niche)<-c("current","MICROC2070rcp26","MICROC2070rcp45", "MICROC2070rcp85")
+
+
+##############################
+#Optional Clip by extent, skip to line 101
+##############################
+ec<-readShapePoly("C:/Users/Ben/Dropbox/Shared Ben and Catherine/FutureAnalog/EcuadorCut.shp")
+r<-raster(extent(ec))
+res(r)<-.1
+plot(ec.r<-rasterize(ec,r))
+niche.crop<-lapply(niche,function(x){
+  r<-crop(raster(x),extent(ec.r))
+filnam<-paste(strsplit(x,".gri")[[1]][1],"crop",sep="")
+  writeRaster(r,filnam,overwrite=TRUE)
+  })
+
+#get the crop files
+niche.crops<-list.files(getwd(),pattern="TotalConsensus_EMbyROCcrop.gri",full.name=T,recursive=T)
+
+#Get current models
+current_niche<-niche.crops[grep("current",niche.crops,value=FALSE)]
+
+#Get future models, for now its just
+MICROC2070rcp26_niche<-niche.crops[grep("MICROC2070rcp26",niche.crops,value=FALSE)]
+MICROC2070rcp85_niche<-niche.crops[grep("MICROC2070rcp85",niche.crops,value=FALSE)]
+MICROC2070rcp45_niche<-niche.crops[grep("MICROC2070rcp45",niche.crops,value=FALSE)]
+
+#create list of input rasters
+input.niche<-list(current_niche,MICROC2070rcp26_niche,MICROC2070rcp45_niche,MICROC2070rcp85_niche)
+
+names(input.niche)<-c("current","MICROC2070rcp26","MICROC2070rcp45", "MICROC2070rcp85")
+#################################################### Start here if not cropping!
+
+#Create siteXspp table from input rasters
+siteXspps<-lapply(input.niche,tableFromRaster,threshold=700)
+
+#Get future models, for now its just
+MICROC2070rcp26_niche<-niche[grep("MICROC2070rcp26",niche,value=FALSE)]
+MICROC2070rcp85_niche<-niche[grep("MICROC2070rcp85",niche,value=FALSE)]
+MICROC2070rcp45_niche<-niche[grep("MICROC2070rcp45",niche,value=FALSE)]
+
+#create list of input rasters
+input.niche<-list(current_niche,MICROC2070rcp26_niche,MICROC2070rcp45_niche,MICROC2070rcp85_niche)
+
+names(input.niche)<-c("current","MICROC2070rcp26","MICROC2070rcp45", "MICROC2070rcp85")
+
+
+#################################################
+#Alpha Cell Statistics - fid the tasxonomic, phylgoenetic and trait diversity at each cell
+#################################################
+#Functions are sourced from AlphaMappingFunctions.R
+
+###create a blank raster object of the correct size and extent to have for projecting the cell values
+blank<-raster(niche.crops[[1]])
+
+#################################
+#########################################
+#######Phylogenetic Alpha Diversity (MPD)
+#########################################
+
+#Remove communities with less than 1 species in a row
+#just get where diversity > 1, there is no phylgoenetic diversity or functional diversity of species with richness = 1
+
+MPDs<-lapply(siteXspps,AlphaPhylo)
+
+########################################
+########## Trait Alpha Tree Diversity (MFD)
+########################################
+
+MFDs<-lapply(siteXspps,AlphaFunc.tree)
+
+###################################################################
+#Functional Dispersion Metric Laliberté, E., and P. Legendre (2010)
+##################################################################
+
+FDs<-lapply(siteXspps,AlphaFunc.FD,traits=mon)
+
+##########################
+#Visualize Mapping Metrics
+##########################
+par(mfrow=c(2,3))
+
+cellVisuals<-function(inp.name){
+
+  #Test with richness
+richness<-cellVis(cells=rownames(siteXspps[names(siteXspps) %in% inp.name][[1]]),value=apply(siteXspps[names(siteXspps) %in% inp.name][[1]],1,sum))
+
+#Phylogenetic
+
+MPD.vis<-cellVis(cells=MPDs[names(MPDs) %in% inp.name][[1]]$Cell,value=MPDs[names(MPDs) %in% inp.name][[1]]$MPD)
+
+#Func Tree
+MFD.vis<-cellVis(MFDs[names(MFDs) %in% inp.name][[1]]$Cell,MFDs[names(MFDs) %in% inp.name][[1]]$MFD)
+return(list(richness,MPD.vis,MFD.vis))}
+
+cell.Rasters<-lapply(names(siteXspps),cellVisuals)
+
+##FD Functional Divergence
+#apply visualization to all columns
+FD_metrics<-lapply(FDs,function(x){
+  apply(x,2,cellVis,cells=rownames(x))
+})
+  
+  
+#create a stack of metrics
+
+for ( x in 1:length(cell.Rasters)){
+  cell.Rasters[[x]][[4]]<-stack(FD_metrics[[x]][-c(1,2,4,8)])
+}
+
+#Normalize the metrics
+FD.norm<-FD.stack/cellStats(FD.stack,"max")
+
+plot(FD.norm)
+
+################################################
+#Calculate differences among climate projections
+################################################
+
+#
+current<-cell.Rasters[[1]]
+
+diff.raster<-lapply(2:length(cell.Rasters),function(x){
+  out<-list(current[[1]]-cell.Rasters[[x]][[1]],
+            current[[2]]-cell.Rasters[[x]][[2]],
+            current[[3]]-cell.Rasters[[x]][[3]],
+current[[4]]-cell.Rasters[[x]][[4]])
+  names(out)<-c("Richness","Phylo","Func")
+  return(stack(out))}
+  )
+
+names(diff.raster)<-names(siteXspps[-1])
+
+par(mfrow=c(3,2))
+plot(diff.raster[[1]],col=brewer.pal(7,"RdBu"))
+plot(diff.raster[[2]],col=brewer.pal(7,"RdBu"))
+
+#####################
+#Correlate rasters
+al<-lapply(1:length(diff.raster),function(x){
+within.cor<-cor(values(diff.raster[[x]]),use="complete.obs")
+within.cor<-melt(within.cor)
+a<-qplot(data=within.cor,x=X1,y=X2,fill=value,geom="tile") + xlab("") + ylab("")+ scale_fill_continuous(low="blue",high="red") + geom_text(aes(label=round(value,2)))
+return(a)
+})
+
+#Write difference raster to file
+writeRaster(diff.raster[[2]],bylayer=TRUE,"C:\\Users\\Ben\\Dropbox\\Shared Ben and Catherine\\FutureAnalog\\Alpha\\AlphaChange.tif",overwrite=TRUE,suffix=names(diff.raster[[2]]))
+
+save.image("C:\\Users\\Ben\\Dropbox\\Lab paper 1 Predicted vs observed assemblages\\AlphaMapping.rData")
---
+++ AlphaMappingFunctions.R
@@ -0,0 +1,666 @@
+
+#Write a function that gets the siteXspp matrix from an input list of niche_model outputs
+tableFromRaster<-function(fil_list,threshold){
+  trial<-foreach(mod=1:length(fil_list),.combine=cbind) %do% {
+    
+    #read in the niche models from file
+    niche.m<-lapply(fil_list[mod],function(x) raster(x))
+    
+    niche_ens<-niche.m[[1]][[1]]
+    
+    #set a threshold for presence/absence
+    A_list<-values(niche_ens>=threshold)*1
+  }
+  
+  #use a regular expression  to extract names
+  species<-str_match(fil_list,pattern="Models/(\\w+.\\w+)")[,2]
+  
+  #Name the rows and columns. 
+  colnames(trial)<-species
+  rownames(trial)<- as.factor(1:nrow(trial))
+  
+  #How many species in a each row?
+  sum.row<-apply(trial,1,sum,na.rm=TRUE)
+  
+  #Get rid of cells with no species
+  siteXspp<-trial[!is.na(sum.row),]
+  siteXspp.raster<-siteXspp[apply(siteXspp,1,sum,na.rm=TRUE)>0,]
+  #return the raster for each siteXspp
+  return(siteXspp.raster)}
+
+#write a function that chunks vectors into pieces
+chunk<-function(dat,max){
+  j <- seq_along(dat)
+  ind<-split(dat, ceiling(j/max))
+  return(ind)
+}
+
+AlphaPhylo<-function(siteXspp.raster){
+    # remove any species not in the phylogeny
+  siteXspp.raster.Phylo<-siteXspp.raster[,colnames(siteXspp.raster) %in% colnames(co)]
+  
+  #Get sum rows
+  rowS.Phylo<-apply(siteXspp.raster.Phylo,1,sum,na.rm=TRUE)
+  
+  #Trim siteXspp
+  siteXspp.Phylo<-siteXspp.raster.Phylo[rowS.Phylo > 1,]
+  
+  #remove the row sum
+  rm(rowS.Phylo)
+  
+  #remove untrimmed siteXspp data
+  rm(siteXspp.raster.Phylo)
+  
+  #mean phylogenetic branch length at each community
+  a<-mpd(siteXspp.Phylo,co)
+  
+  #Add in cell names and write to file
+  MPDcell<-data.frame(rownames(siteXspp.Phylo),a)
+  colnames(MPDcell)<-c("Cell","MPD")
+  return(MPDcell)}
+
+AlphaFunc.tree<-function(siteXspp.raster){
+  
+  ftrx<-prune.sample(siteXspp.raster,tree.func)
+  fco<-cophenetic(ftrx)
+  
+  #Only keep communities with species with functional information and richness > 1
+  siteXspp.raster.Func<-siteXspp.raster[,colnames(fco)]
+  rowS.Func<-apply(siteXspp.raster.Func,1,sum)
+  siteXspp.Func<-siteXspp.raster.Func[rowS.Func > 1,]
+  
+  #find mean func neighbor at each assemblage
+  a<-mpd(siteXspp.Func,fco)
+  
+  #Put the cell numbers so it can be identified as each community
+  MFDCell<-data.frame(rownames(siteXspp.Func),a)
+  colnames(MFDCell)<-c("Cell","MFD")
+  
+  return(MFDCell)
+}
+
+#Start with the three trait data, means we need more than 2 species 
+
+#Create FD compute function
+
+AlphaFunc.FD<-function(siteXspp.raster,traits){
+    
+  #Need more species than traits
+  siteXspp.FD<-siteXspp.raster[,colnames(siteXspp.raster) %in% gsub(" ","\\.",traits$Species)]
+  rowS.Func<-apply(siteXspp.FD,1,sum)
+  siteXspp.FD<-siteXspp.raster[rowS.Func > ncol(traits),]
+  
+  #Run FD metric on each row in the raster
+  cl<-makeCluster(8,"SOCK")
+  registerDoSNOW(cl)
+  
+  trait_FD<-foreach(h=1:nrow(siteXspp.FD),.packages="FD",.errorhandling="pass",.combine=rbind) %dopar% {
+    g<-siteXspp.FD[h,] 
+    
+    #Which species are present in the community
+    f<-names(g[which(g==1)])
+    
+    #For each community apply the functional metrics
+    trait.site<-traits[traits$Species %in% gsub("\\."," ",f),]
+    
+    #make species rownames
+    rownames(trait.site)<-trait.site[,1]
+    trait.site<-trait.site[,-1]
+    
+    trait.output<-dbFD(trait.site,calc.CWM=FALSE,messages=TRUE)
+    trait.out<-as.data.frame(trait.output)
+    
+    return(trait.out)}
+  
+  stopCluster(cl)
+  
+  rownames(trait_FD)<-rownames(siteXspp.FD)
+  return(trait_FD)}
+
+cellVis<-function(cells,value){
+  #create a empty raster the exact dimensions of the model, this needs to match extent and grain first
+  alpha_structure<-raster(blank)
+  values(alpha_structure)<-NA
+  
+  alpha_structure[as.numeric(as.character(cells))]<-value
+  names(alpha_structure)<-NULL
+  plot(alpha_structure)
+  
+  return(alpha_structure)
+}
+
+###Parallel Phylosor
+
+#create parallel phylobeta functions
+PD.par<-function (samp, tree, include.root = TRUE) 
+{
+  if (is.null(tree$edge.length)) {
+    stop("Tree has no branch lengths, cannot compute pd")
+  }
+  species <- colnames(samp)
+  SR <- rowSums(ifelse(samp > 0, 1, 0))
+  nlocations = dim(samp)[1]
+  nspecies = dim(samp)[2]
+  PDs = NULL
+  PDs<-foreach (i=1:nlocations,.combine=c,.packages="picante") %dopar% {
+    present <- species[samp[i, ] > 0]
+    treeabsent <- tree$tip.label[which(!(tree$tip.label %in% 
+                                           present))]
+    if (length(present) == 0) {
+      PD <- 0
+    }
+    else if (length(present) == 1) {
+      if (!is.rooted(tree) || !include.root) {
+        warning("Rooted tree and include.root=TRUE argument required to calculate PD of single-species sampunities. Single species sampunity assigned PD value of NA.")
+        PD <- NA
+      }
+      else {
+        PD <- node.age(tree)$ages[which(tree$edge[, 
+                                                  2] == which(tree$tip.label == present))]
+      }
+    }
+    else if (length(treeabsent) == 0) {
+      PD <- sum(tree$edge.length)
+    }
+    else {
+      sub.tree <- drop.tip(tree, treeabsent)
+      if (include.root) {
+        if (!is.rooted(tree)) {
+          stop("Rooted tree required to calculate PD with include.root=TRUE argument")
+        }
+        sub.tree.depth <- max(node.age(sub.tree)$ages)
+        orig.tree.depth <- max(node.age(tree)$ages[which(tree$edge[, 
+                                                                   2] %in% which(tree$tip.label %in% present))])
+        PD <- sum(sub.tree$edge.length) + (orig.tree.depth - 
+                                             sub.tree.depth)
+      }
+      else {
+        PD <- sum(sub.tree$edge.length)
+      }
+    }
+    return(PD)
+  }
+  PDout <- data.frame(PD = PDs, SR = SR)
+  rownames(PDout) <- rownames(samp)
+  return(PDout)
+}
+
+par.phylosor<-function (samp, tree) 
+{
+  if (is.null(tree$edge.length)) {
+    stop("Tree has no branch lengths, cannot compute pd")
+  }
+  if (!is.rooted(tree)) {
+    stop("Rooted phylogeny required for phylosor calculation")
+  }
+  samp <- as.matrix(samp)
+  s <- nrow(samp)
+  phylodist <- matrix(NA, s, s)
+  rownames(phylodist) <- rownames(samp)
+  colnames(phylodist) <- rownames(samp)
+  samp_comb <- matrix(NA, s * (s - 1)/2, ncol(samp))
+  colnames(samp_comb) <- colnames(samp)
+  i <- 1
+  for (l in 1:(s - 1)) {
+    for (k in (l + 1):s) {
+      samp_comb[i, ] <- samp[l, ] + samp[k, ]
+      i <- i + 1
+    }
+  }
+  pdsamp <- PD.par(samp, tree)
+  pdsamp_comb <- PD.par(samp_comb, tree)
+  i <- 1
+  for (l in 1:(s - 1)) {
+    pdl <- pdsamp[l, "PD"]
+    for (k in (l + 1):s) {
+      pdk <- pdsamp[k, "PD"]
+      pdcomb <- pdsamp_comb[i, "PD"]
+      pdsharedlk <- pdl + pdk - pdcomb
+      phylodist[k, l] = 2 * pdsharedlk/(pdl + pdk)
+      i <- i + 1
+    }
+  }
+  return(as.dist(phylodist))
+}
+
+# phyl = phylo object, com = site by species matrix
+
+# phyl = phylo object, com = site by species matrix
+
+matpsim <- function(phyl, com, clust = 7) # make sure nodes are labelled and that com and phyl species match
+{
+  
+  require(phylobase)  # detail information for all phylo branches
+  new <- phylo4(phyl)
+  dat <- as.data.frame(print(new))
+  allbr <- dat$edge.length
+  names(allbr) <- getEdge(new)
+  spp <- colnames(com)
+  
+  require(foreach)
+  require(doSNOW)
+  
+  cl <- makeCluster(clust) # create parellel clusters
+  registerDoSNOW(cl)
+  
+  # create a list of phy branches for each species
+  
+  brs <-  foreach(i = spp, .packages = "phylobase") %dopar% #this loop makes a list of branches for each species
+{  
+  print(which(spp == i)/length(spp))
+  print(date())
+  brsp <- vector()
+  br   <- as.numeric(rownames(dat[which(dat$label==i),]))
+  repeat{
+    brsn <- getEdge(new,br)
+    brsl <- dat[names(brsn),"edge.length"]
+    names(brsl) <- brsn
+    brsp <- c(brsp, brsl)
+    br   <- dat[br,3]
+    if(br == 0) {break}
+  }
+  
+  brsp
+}
+  names(brs) <- spp
+  stopCluster(cl)
+  
+    
+  print("brs")
+  
+  # create a species by phy branch matrix
+  
+  spp_br <- matrix(0,nrow = length(spp), ncol = length(allbr))
+  rownames(spp_br) <- spp
+  colnames(spp_br) <- names(allbr)
+  
+  for(i in spp)
+  {
+    spp_br[i,names(brs[[i]])] <- brs[[i]]
+  }
+  
+  spp_br <- spp_br[,-(ncol(com)+1)] # removes root
+  spp_br <- spp_br[,!colSums(spp_br) %in% c(0,nrow(spp_br))]  # here take out all common branches instead
+  
+  print("spp_br")
+  
+  spp_br <<- spp_br
+  
+  # function to give the pres/abs of phy branches withi cell i
+  
+  cellbr <- function(i,spp_br, com)
+  {
+    i_spp <- rownames(spp_br)[com[i,]>0]
+    if(length(i_spp) > 1)
+    {i_br  <- as.numeric(apply(spp_br[i_spp,],2,max))}
+    else  {i_br  <- as.numeric(spp_br[i_spp,]) }
+    names(i_br) <- colnames(spp_br)
+    return(i_br)
+  }
+  
+  require(foreach)
+  require(doSNOW)
+  
+  cl <- makeCluster(clust) # create parellel clusters
+  registerDoSNOW(cl)
+  
+  tcellbr <- foreach(j = rownames(com), .combine = "rbind") %dopar% {cellbr(j,spp_br,com)}
+  
+  stopCluster(cl)
+  
+  print("cell_br")
+  rownames(tcellbr) <- rownames(com)
+  tcellbr <<- tcellbr
+  
+  # function to calculate phylobsim between cell_a and cell_b
+  
+  nmatsim <- function(cell_b) # samp = grid cell of interest
+  {
+    a_br  <- tcellbr[cell_a,]
+    b_br <- tcellbr[cell_b,]
+    s_br <- rbind(a_br,b_br)
+    s_br <- as.matrix(s_br[,colSums(s_br) >0])
+    pa_br <- s_br > 0
+    both <- s_br[1,colSums(pa_br > 0)==2]
+    ubr <- s_br[,colSums(pa_br > 0)==1]
+    a_ubr <- as.matrix(ubr)[1,]
+    b_ubr <- as.matrix(ubr)[2,]
+    psim <- 1 - (sum(both,na.rm=T)/(min(sum(a_ubr,na.rm=T),sum(b_ubr,na.rm=T))+sum(both,na.rm=T)))
+    return(psim)
+  }
+  
+  # calculate full cell by cell phylobsim matrix
+  
+  cl <- makeCluster(clust) # make another cluster
+  registerDoSNOW(cl)
+  
+  psim <- foreach(j = rownames(tcellbr)) %dopar% {
+    print(j);cell_a <- j; 
+    unlist(
+      lapply(
+        rownames(tcellbr)[1:(which(rownames(tcellbr) == j))], 
+        nmatsim
+      )
+    )
+  }
+  
+  stopCluster(cl)
+  print("psim")
+  psim <- do.call("rbind", psim)
+  rownames(psim) <- rownames(tcellbr)
+  colnames(psim) <- rownames(tcellbr)    
+  psim[upper.tri(psim)] <- psim[lower.tri(psim)]
+  psim <- as.dist(psim)
+  print("its over")
+  return(psim)
+  }
+
+######################
+#Non parallel version
+#####################
+matpsim.nopar <- function(phyl, com, clust = 7) # make sure nodes are labelled and that com and phyl species match
+{
+  
+  require(phylobase)  # detail information for all phylo branches
+  new <- phylo4(phyl)
+  dat <- as.data.frame(print(new))
+  allbr <- dat$edge.length
+  names(allbr) <- getEdge(new)
+  spp <- colnames(com)
+  
+  require(foreach)
+  require(doSNOW)
+  
+  #cl <- makeCluster(clust) # create parellel clusters
+  #registerDoSNOW(cl)
+  
+  # create a list of phy branches for each species
+  
+  brs <-  foreach(i = spp, .packages = "phylobase") %do% #this loop makes a list of branches for each species
+{  
+  print(which(spp == i)/length(spp))
+  print(date())
+  brsp <- vector()
+  br   <- as.numeric(rownames(dat[which(dat$label==i),]))
+  repeat{
+    brsn <- getEdge(new,br)
+    brsl <- dat[names(brsn),"edge.length"]
+    names(brsl) <- brsn
+    brsp <- c(brsp, brsl)
+    br   <- dat[br,3]
+    if(br == 0) {break}
+  }
+  
+  brsp
+}
+  names(brs) <- spp
+  #stopCluster(cl)
+  
+  
+  print("brs")
+  
+  # create a species by phy branch matrix
+  
+  spp_br <- matrix(0,nrow = length(spp), ncol = length(allbr))
+  rownames(spp_br) <- spp
+  colnames(spp_br) <- names(allbr)
+  
+  for(i in spp)
+  {
+    spp_br[i,names(brs[[i]])] <- brs[[i]]
+  }
+  
+  spp_br <- spp_br[,-(ncol(com)+1)] # removes root
+  spp_br <- spp_br[,!colSums(spp_br) %in% c(0,nrow(spp_br))]  # here take out all common branches instead
+  
+  print("spp_br")
+  
+  spp_br <<- spp_br
+  
+  # function to give the pres/abs of phy branches withi cell i
+  
+  cellbr <- function(i,spp_br, com)
+  {
+    i_spp <- rownames(spp_br)[com[i,]>0]
+    if(length(i_spp) > 1)
+    {i_br  <- as.numeric(apply(spp_br[i_spp,],2,max))}
+    else  {i_br  <- as.numeric(spp_br[i_spp,]) }
+    names(i_br) <- colnames(spp_br)
+    return(i_br)
+  }
+  
+  require(foreach)
+  require(doSNOW)
+  
+  #cl <- makeCluster(clust) # create parellel clusters
+  #registerDoSNOW(cl)
+  
+  tcellbr <- foreach(j = rownames(com), .combine = "rbind") %do% {cellbr(j,spp_br,com)}
+  
+  #stopCluster(cl)
+  
+  print("cell_br")
+  rownames(tcellbr) <- rownames(com)
+  tcellbr <<- tcellbr
+  
+  # function to calculate phylobsim between cell_a and cell_b
+  
+  nmatsim <- function(cell_b) # samp = grid cell of interest
+  {
+    a_br  <- tcellbr[cell_a,]
+    b_br <- tcellbr[cell_b,]
+    s_br <- rbind(a_br,b_br)
+    s_br <- as.matrix(s_br[,colSums(s_br) >0])
+    pa_br <- s_br > 0
+    both <- s_br[1,colSums(pa_br > 0)==2]
+    ubr <- s_br[,colSums(pa_br > 0)==1]
+    a_ubr <- as.matrix(ubr)[1,]
+    b_ubr <- as.matrix(ubr)[2,]
+    psim <- 1 - (sum(both,na.rm=T)/(min(sum(a_ubr,na.rm=T),sum(b_ubr,na.rm=T))+sum(both,na.rm=T)))
+    return(psim)
+  }
+  
+  # calculate full cell by cell phylobsim matrix
+  
+  #cl <- makeCluster(clust) # make another cluster
+  #registerDoSNOW(cl)
+  
+  psim <- foreach(j = rownames(tcellbr)) %do% {
+    print(j);cell_a <- j; 
+    unlist(
+      lapply(
+        rownames(tcellbr)[1:(which(rownames(tcellbr) == j))], 
+        nmatsim
+      )
+    )
+  }
+  
+  #stopCluster(cl)
+  print("psim")
+  psim <- do.call("rbind", psim)
+  rownames(psim) <- rownames(tcellbr)
+  colnames(psim) <- rownames(tcellbr)    
+  psim[upper.tri(psim)] <- psim[lower.tri(psim)]
+  psim <- as.dist(psim)
+  print("its over")
+  return(psim)
+}
+
+
+##################
+#Pairwise Version
+##################
+
+matpsim.pairwise <- function(phyl, com.x, com.y, clust = 7) # make sure nodes are labelled and that com and phyl species match
+{
+  
+  require(phylobase)  # detail information for all phylo branches
+  new <- phylo4(phyl)
+  dat <- as.data.frame(print(new))
+  allbr <- dat$edge.length
+  names(allbr) <- getEdge(new)
+  #Assumes same species in the comm matrices
+  spp <- colnames(com.x)
+  
+  require(foreach)
+  require(doSNOW)
+  
+  cl <- makeCluster(clust) # create parellel clusters
+  registerDoSNOW(cl)
+  
+  # create a list of phy branches for each species
+  
+  brs <-  foreach(i = spp, .packages = "phylobase") %dopar% #this loop makes a list of branches for each species
+{  
+  print(which(spp == i)/length(spp))
+  print(date())
+  brsp <- vector()
+  br   <- as.numeric(rownames(dat[which(dat$label==i),]))
+  repeat{
+    brsn <- getEdge(new,br)
+    brsl <- dat[names(brsn),"edge.length"]
+    names(brsl) <- brsn
+    brsp <- c(brsp, brsl)
+    br   <- dat[br,3]
+    if(br == 0) {break}
+  }
+  
+  brsp
+}
+  names(brs) <- spp
+  stopCluster(cl)
+  
+  
+  print("brs")
+  
+  # create a species by phy branch matrix
+  
+  spp_br <- matrix(0,nrow = length(spp), ncol = length(allbr))
+  rownames(spp_br) <- spp
+  colnames(spp_br) <- names(allbr)
+  
+  for(i in spp)
+  {
+    spp_br[i,names(brs[[i]])] <- brs[[i]]
+  }
+  
+  spp_br <- spp_br[,-(ncol(com.x)+1)] # removes root
+  spp_br <- spp_br[,!colSums(spp_br) %in% c(0,nrow(spp_br))]  # here take out all common branches instead
+  
+  print("spp_br")
+  
+  spp_br <<- spp_br
+  
+  # function to give the pres/abs of phy branches withi cell i
+  
+  cellbr <- function(i,spp_br, com)
+  {
+    i_spp <- rownames(spp_br)[com[i,]>0]
+    if(length(i_spp) > 1)
+    {i_br  <- as.numeric(apply(spp_br[i_spp,],2,max))}
+    else  {i_br  <- as.numeric(spp_br[i_spp,]) }
+    names(i_br) <- colnames(spp_br)
+    return(i_br)
+  }
+  
+  require(foreach)
+  require(doSNOW)
+  
+  cl <- makeCluster(clust) # create parellel clusters
+  registerDoSNOW(cl)
+  
+  #Looks like we need to duplicate this function! Once for the current matrix, and one for the future matrix
+  
+  tcellbr.x <- foreach(j = rownames(com.x), .combine = "rbind") %dopar% {cellbr(j,spp_br,com.x)}
+  tcellbr.y <- foreach(j = rownames(com.y), .combine = "rbind") %dopar% {cellbr(j,spp_br,com.y)}
+  
+  stopCluster(cl)
+  
+  print("cell_br")
+  rownames(tcellbr.x) <- rownames(com.x)
+  rownames(tcellbr.y) <- rownames(com.y)
+  tcellbr <<- tcellbr
+  
+  # function to calculate phylobsim between cell_a and cell_b
+  
+  nmatsim <- function(cell_a,cell_b) # samp = grid cell of interest
+  {
+    a_br  <- tcellbr.x[cell_a,]
+    b_br <- tcellbr.y[cell_b,]
+    s_br <- rbind(a_br,b_br)
+    s_br <- as.matrix(s_br[,colSums(s_br) >0])
+    pa_br <- s_br > 0
+    both <- s_br[1,colSums(pa_br > 0)==2]
+    ubr <- s_br[,colSums(pa_br > 0)==1]
+    a_ubr <- as.matrix(ubr)[1,]
+    b_ubr <- as.matrix(ubr)[2,]
+    psim <- 1 - (sum(both,na.rm=T)/(min(sum(a_ubr,na.rm=T),sum(b_ubr,na.rm=T))+sum(both,na.rm=T)))
+    return(psim)
+  }
+  
+  # calculate full cell by cell phylobsim matrix
+  
+  cl <- makeCluster(clust) # make another cluster
+  registerDoSNOW(cl)
+  
+  psim<-foreach(j=rownames(tcellbr.x)) %dopar%{
+    sapply(rownames(tcellbr.y),function(k){
+      nmatsim(j,k)
+  })}
+
+  
+  stopCluster(cl)
+  
+  print("psim")
+  psim <- do.call("rbind", psim)
+  rownames(psim) <- rownames(tcellbr.x)
+  colnames(psim) <- rownames(tcellbr.y)    
+  print("its over")
+  return(psim)
+}
+
+# hierachical clustering eval
+
+Beval.meth <- function(m.list, dismat, s.all,maxclust = 100)
+{
+  
+  cl <- makeCluster(4)
+  registerDoSNOW(cl)
+  
+  Beval <- function (clusts, dismat, s.all) {
+    bBeta <- foreach(i = unique(clusts), .combine = "c")%do%{dismat[names(clusts[clusts %in% unique(clusts)[-c(1:which(unique(clusts) == i))]]),names(clusts[clusts == i])]}
+    eval <- 1-(sum(bBeta)/s.all)
+    return(eval)
+  }
+  
+  all.eval <- foreach(m = m.list, .packages = "foreach", .combine = "rbind")%dopar%
+{
+  foreach(i = 2:maxclust, .combine = "c")%do%{gc();Beval(cutree(m,i),dismat,s.all)}
+  
+}
+  stopCluster(cl)
+  return(all.eval) 
+}
+
+
+# non-hierachical clustering eval
+
+Beval.pam <- function( dis, dismat, s.all,maxclust = 100)
+{
+  
+  cl <- makeCluster(4)
+  registerDoSNOW(cl)
+  
+  Beval <- function (clusts, dismat, s.all) {
+    bBeta <- foreach(i = unique(clusts)[-length(unique(clusts))], .combine = "c")%do%{dismat[names(clusts[clusts %in% unique(clusts)[-c(1:which(unique(clusts) == i))]]),names(clusts[clusts == i])]}
+    eval <- 1-(sum(bBeta)/s.all)
+    return(eval)
+  }
+  
+  all.eval <- foreach(i = 2:(maxclust-1), .combine = "c", .packages = c("cluster","foreach"))%do%{gc();Beval(pam(dis,i,diss = T)$clustering,dismat,s.all)}
+  
+  
+  stopCluster(cl)
+  return(all.eval) 
+}
+

fad25a98ec388d0a8e139db179a2eee2fa9e1ed7
Sarah Supp <sarah@weecology.org>
2014-02-21 12:38:42 -0500

    added docstrings for the MNND function

--- BenHolttraitDiversity.R
+++ BenHolttraitDiversity.R
@@ -1,5 +1,8 @@
-MNND <- function(A,B,sp.list,dists)
-{
+MNND <- function(A,B,sp.list,dists){
+# Based on Holt et al. 2013. An Update of Wallace’s Zoogeographic Regions of the World. Science 339: 74-78.
+# Supplemental Material : http://www.jeanphilippelessard.com/Jean-Philippe_Lessard/Publications_files/Holt.SM.pdf
+# Inputs an iterator (A), B, the species list (which could be traits or phylo) and a distance matrix
+# Outputs mean nearest neighbor taxon distance
 
 Asp     <- sp.list[[A]]
 Bsp     <- sp.list[[B]]

7f4fecd251f7367195e0486b969e09ae7470f33a
lorra <lgxlg3@nottingham.ac.uk>
2015-06-03 15:40:43 +0100

    new projections code which doesn't rely on explicitly naming the climate scenario

--- runProjections.R
+++ runProjections.R
@@ -1,65 +1,73 @@
 # runProjections.R -------------------------------------------------------------
-# This code will run the projections for any GCM put as input. 
-# Step 3) Climate Scenarios and Futute Climate #################################
-# TO DO: Once code is working fully, update the climate scenarios loading to
-# collate all climate scenarios in the folder - this will involve searching the
-# "worldclim_data" folder, creating a list, and 'listifying' all of the climate
-# layer bits and pieces. Should be straightforward and will make automation
-# easier. Also, think about removing climate scenarios which have already been
-# run from the list?
+# This code will run the projections for any GCM / Species combination for which
+# the projections have not already been created.
 
-# Bring in future climate layers (change here to add in more scenarios - all
-# below code will need adapting to include new scenarios)
-# Modelname_year_emmissionscenario
-MICROC_2070_rcp26 <- stack("../worldclim_data/mc26bi70/mc26bi701.tif",
-                           "../worldclim_data/mc26bi70/mc26bi7012.tif",
-                           "../worldclim_data/mc26bi70/mc26bi7015.tif")
-MICROC_2070_rcp85 <- stack("../worldclim_data/mc85bi70/mc85bi701.tif",
-                           "../worldclim_data/mc85bi70/mc85bi7012.tif",
-                           "../worldclim_data/mc85bi70/mc85bi7015.tif")
-MICROC_2070_rcp45 <- stack("../worldclim_data/mc45bi70/mc45bi701.tif",
-                           "../worldclim_data/mc45bi70/mc45bi7012.tif",
-                           "../worldclim_data/mc45bi70/mc45bi7015.tif")
+# load required packages (installing if not already done) ----------------------
+packages <- c("raster", "dplyr", "biomod2")
 
-# make the names consistent for all 
-names(MICROC_2070_rcp26) <- names(myExpl)
-names(MICROC_2070_rcp85) <- names(myExpl)
-names(MICROC_2070_rcp45) <- names(myExpl)
-
-
-
-# Step 4) Set the Extent to project *into*. ####################################
-
-# Presence points are still taken from everywhere Avoid projecting into areas 
-# where sample size is really low **DECISION**
-exte<-extent(c(-81.13411,-68.92061,-5.532386,11.94902))
-
-# Cut by the extent. Crop by this layer 
-myExpl.crop<-stack(crop(myExpl,exte))
-MICROC_2070_rcp26.c<-stack(crop(MICROC_2070_rcp26,exte))
-MICROC_2070_rcp85.c<-stack(crop(MICROC_2070_rcp85,exte))
-MICROC_2070_rcp45.c<-stack(crop(MICROC_2070_rcp45,exte))
-
-# set resolution for the future layers equivalent to cell size
-if (!res(MICROC_2070_rcp26)[[1]] == cell_size){
-  fact1<-cell_size/res(MICROC_2070_rcp26.c)
-  fact2<-cell_size/res(MICROC_2070_rcp85.c)
-  fact3<-cell_size/res(MICROC_2070_rcp45.c)
-  
-  # Set cell size to ~ cell_size degree
-  MICROC_2070_rcp26.c<-stack(aggregate(MICROC_2070_rcp26.c,fact1))
-  MICROC_2070_rcp85.c<-stack(aggregate(MICROC_2070_rcp85.c,fact2))
-  MICROC_2070_rcp45.c<-stack(aggregate(MICROC_2070_rcp45.c,fact3))
+for(p in packages) {
+  if (!p %in% installed.packages()) {
+    install.packages(p)
+  }
+  require(p, character.only = TRUE)
 }
 
-# create a list of all env to project into
-projEnv<-list(myExpl.crop,MICROC_2070_rcp26.c,MICROC_2070_rcp45.c,MICROC_2070_rcp85.c)
-names(projEnv)<-c("current","MICROC2070rcp26","MICROC2070rcp45","MICROC2070rcp85")
+# load fnSDM.R
+source("fnSDM.R")
 
 
+# set cell size
+cell_size = 0.1
 
+output_folder = "../FutureAnalog_output" 
+out_path <- paste(output_folder, cell_size, sep = "/")
 
+# Step 1) List the future climate data available and collate variables ---------
+# Create list of climate models
+clim.mods <- list.files("../worldclim_data/projections_2070/")
 
-for (i in 1:length(projEnv)){
-  bio_project(projEnv[[i]], names(projEnv[i]))
+for (mod in clim.mods) {
+  # get the required climate variables
+  mod.var1 <- list.files(paste("../worldclim_data/projections_2070/", mod, sep="/"),
+                         pattern = "701.tif$", full.names = TRUE)
+  
+  mod.var12 <- list.files(paste("../worldclim_data/projections_2070/", mod, sep="/"),
+                         pattern = "7012.tif$", full.names = TRUE)
+  
+  mod.var15 <- list.files(paste("../worldclim_data/projections_2070/", mod, sep="/"),
+                         pattern = "7015.tif$", full.names = TRUE)
+  
+  mod.stack <- stack(mod.var1, mod.var12, mod.var15)
+  
+  # make the names consistent
+  names(mod.stack) <- c("bio_1", "bio_12", "bio_15")
+  
+  # Step 2) Set the Extent to project *into*. ----------------------------------
+  
+  # Presence points are still taken from everywhere Avoid projecting into areas 
+  # where sample size is really low **DECISION**
+  exte<-extent(c(-81.13411,-68.92061,-5.532386,11.94902))
+  
+  # Cut by the extent. Crop by this layer 
+  mod.stack.c <- stack(crop(mod.stack, exte))
+  
+  # set resolution for the future layers equivalent to cell size
+  if (!res(mod.stack.c)[[1]] == cell_size){
+    fact <- cell_size/res(mod.stack.c)
+    # Set cell size to ~ cell_size degree
+    mod.stack.c <- stack(aggregate(mod.stack.c, fact))
+  }
+  
+  # Step 3) Get a list of the species which have SDMs already ------------------
+  setwd(out_path)
+  spec <- list.files(".")
+  spec <- spec[file.info(spec)$isdir]
+  spec <- spec[!grepl("logs",spec)] # don't want the logs folder included
+  
+  # Step 4) Create projections into future climate -----------------------------
+  for (sp in spec){
+    # project into future climate (bio_project() defined in fnSDM.R)
+    if(!file.exists(paste0(sp, "/proj_", mod))) bio_project(sp, mod.stack.c, mod)
+  }
+  setwd("../../FutureAnalog/")
 }

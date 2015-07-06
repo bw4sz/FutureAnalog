85be353fbf25e6f904eab96399c45e787e22dd9e
lorra <lgxlg3@nottingham.ac.uk>
2015-07-06 15:14:21 +0100

    Add in code to create biplots of traits

---
+++ TraitMapping.R
@@ -0,0 +1,65 @@
+# get list of sppXsite matrices for each environmental scenario
+sppXsite.files <- list.files("sppXsite", full.names = TRUE)
+
+# get the number/proportion of cells occupied for each species in each climate scenario
+sXs.summary <- list()
+for(sppXsite.file in sppXsite.files){
+  load(sppXsite.file)
+  
+  # get climate model name
+  clim.mod <- str_match(sppXsite.file,pattern=paste("sppXsite/(\\w+.\\w+).rda",sep="/"))[,2]
+  
+  # get rid of cell information
+  sppXsite <- sppXsite[,2:(ncol(sppXsite)-3)]
+  fails <- na.test(sppXsite)
+  sppXsite <- sppXsite[,!colnames(sppXsite) %in% fails]
+  sXs.summary[[clim.mod]] <- data.frame(species=colnames(sppXsite), no.cells=colSums(sppXsite), 
+                            perc.occ=colSums(sppXsite)/nrow(sppXsite), clim.mod=clim.mod)
+}
+
+sXs.summary <- do.call("rbind", sXs.summary)
+
+# load trait data
+traits <- getTraitData()
+
+# remove species from summary where we do not have trait data and vice versa
+sXs.summary <- filter(sXs.summary, species %in% rownames(traits))
+traits <- filter(traits, rownames(traits) %in% sXs.summary$species)
+
+# separate out the current from future scenario results
+sXs.current <- filter(sXs.summary, clim.mod=="current")
+sXs.future <- filter(sXs.summary, clim.mod!="current")
+
+# separate the GCM and RCP in the scenario identifier
+sXs.future$GCM <- substr(sXs.future$clim.mod, 1, 2)
+sXs.future$RCP <- substr(sXs.future$clim.mod, 3, 4)
+
+# calculate the difference in proportion of cells occupied between the current
+# and all future scenarios
+sXs.future$diff <- sXs.current$perc.occ - sXs.future$perc.occ
+
+# create data frames to be used for groups, point size and facets in the biplot
+sXs.RCP <- group_by(sXs.future, RCP, species) %>%
+  summarise(diff=mean(diff)) %>%
+  mutate(groups=ifelse(diff < 0, "More cells occupied in future environment", "More cells occupied in current environment"),
+         diff=abs(diff),
+         scenario=RCP)
+
+sXs.GCM <- group_by(sXs.future, GCM, species) %>%
+  summarise(diff=mean(diff)) %>%
+  mutate(groups=ifelse(diff < 0, "More cells occupied in future environment", "More cells occupied in current environment"),
+         diff=abs(diff),
+         scenario=GCM)
+
+# PCA on the species traits
+pca <- prcomp(traits)
+
+# plots
+ggbiplot2(pca, add.dat = sXs.RCP) + theme_classic() +
+  labs(size="Absolute difference in % cells occupied", color="")
+ggsave("Figures/traitsbyRCP.pdf", width=17, height=9)
+
+ggbiplot2(pca, add.dat = sXs.GCM) + theme_classic() +
+  labs(size="Absolute difference in % cells occupied", color="") + 
+  theme(legend.position = c(1, 0), legend.justification = c(1, 0)) 
+ggsave("Figures/traitsbyGCM.pdf", width=17, height=9)
--- TraitMappingFunctions.R
+++ TraitMappingFunctions.R
@@ -73,3 +73,91 @@ getTraitData <- function() {
   rownames(z.scores) <- rownames(mon)
   return(z.scores)
 }
+
+ggbiplot2 <- function (pcobj, add.dat, choices = 1:2, scale = 1, pc.biplot = TRUE, 
+                       obs.scale = 1 - scale, var.scale = scale, groups = NULL, 
+                       ellipse = FALSE, ellipse.prob = 0.68, labels = NULL, labels.size = 3, 
+                       alpha = 1, pt.size, var.axes = TRUE, circle = FALSE, circle.prob = 0.69, 
+                       varname.size = 3, varname.adjust = 1.5, varname.abbrev = FALSE, 
+                       ...) 
+{
+  library(ggplot2)
+  library(plyr)
+  library(scales)
+  library(grid)
+  
+  nobs.factor <- sqrt(nrow(pcobj$x) - 1)
+  d <- pcobj$sdev
+  u <- sweep(pcobj$x, 2, 1/(d * nobs.factor), FUN = "*")
+  v <- pcobj$rotation
+  
+  choices <- pmin(choices, ncol(u))
+  df.u <- as.data.frame(sweep(u[, choices], 2, d[choices]^obs.scale, 
+                              FUN = "*"))
+  v <- sweep(v, 2, d^var.scale, FUN = "*")
+  df.v <- as.data.frame(v[, choices])
+  names(df.u) <- c("xvar", "yvar")
+  names(df.v) <- names(df.u)
+  
+  df.u <- df.u * nobs.factor
+  
+  r <- sqrt(qchisq(circle.prob, df = 2)) * prod(colMeans(df.u^2))^(1/4)
+  v.scale <- rowSums(v^2)
+  df.v <- r * df.v/sqrt(max(v.scale))
+  if (obs.scale == 0) {
+    u.axis.labs <- paste("standardized PC", choices, sep = "")
+  } else {
+    u.axis.labs <- paste("PC", choices, sep = "")
+  }
+  u.axis.labs <- paste(u.axis.labs, sprintf("(%0.1f%% explained var.)", 
+                                            100 * pcobj$sdev[choices]^2/sum(pcobj$sdev^2)))
+  
+  # get the additional data onto the PCA scores dataframe
+  df.u <- cbind(df.u, add.dat)
+  
+  # create the dataframe with the variable axes
+  df.v$varname <- rownames(v)
+  df.v$angle <- with(df.v, (180/pi) * atan(yvar/xvar))
+  df.v$hjust = with(df.v, (1 - varname.adjust * sign(xvar))/2)
+  
+  # create ellipse
+  theta <- c(seq(-pi, pi, length = 50), seq(pi, -pi, length = 50))
+  circle <- cbind(cos(theta), sin(theta))
+  ell <- ddply(df.u, "groups", function(x) {
+    if (nrow(x) <= 2) {
+      return(NULL)
+    }
+    sigma <- var(cbind(x$xvar, x$yvar))
+    mu <- c(mean(x$xvar), mean(x$yvar))
+    ed <- sqrt(qchisq(ellipse.prob, df = 2))
+    data.frame(sweep(circle %*% chol(sigma) * ed, 2, 
+                     mu, FUN = "+"), groups = x$groups[1])
+  })
+  names(ell)[1:2] <- c("xvar", "yvar")
+  
+  
+  # plot it all
+  g <- ggplot(data = df.u, aes(x = xvar, y = yvar)) + 
+    #scale_x_continuous(limits=c(0, 2.5)) + 
+    #scale_y_continuous(limits=c(-2.5, 2)) + 
+    xlab(u.axis.labs[1]) + 
+    ylab(u.axis.labs[2]) + 
+    coord_equal()
+  
+  
+  g <- g + geom_point(aes(size = diff, color = groups), alpha = alpha)
+  
+  g <- g + geom_path(data = ell, aes(color = groups, group = groups))
+  
+  g <- g + geom_segment(data = df.v, 
+                        aes(x = 0, y = 0, xend = xvar, yend = yvar), 
+                        arrow = arrow(length = unit(1/2, "picas")), color = muted("red"))
+  
+  g <- g + geom_text(data = df.v, 
+                     aes(label = varname, x = xvar, y = yvar, angle = angle, hjust = hjust), 
+                     color = "darkred", size = varname.size)
+  
+  g <- g + facet_wrap(~scenario, nrow=floor(length(unique(add.dat$scenario))/2))
+  
+  return(g)
+}

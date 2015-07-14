5ba5d5c99e564ab7322c68cdd0486dc8834f4a08
lorra <lgxlg3@nottingham.ac.uk>
2015-07-14 20:19:25 +0100

    tidy plot code and comment out scenario comparison for now

--- runFAPlots.R
+++ runFAPlots.R
@@ -31,6 +31,15 @@ dat$GCM <- factor(dat$GCM, levels=unique(dat$GCM),
                   labels=c("CCSM4", "CNRM-CM5", "GISS-E2-R", "HadGEM2-ES", 
                            "IPSL-CM5A-LR", "MIROC5", "MPI-ESM-LR"))
 
+# hillshade data
+load("InputData/srtm_5arcmin.rda")
+slope = terrain(elev, opt='slope')
+aspect = terrain(elev, opt='aspect')
+hill = hillShade(slope, aspect, 40, 270)
+hdf <- rasterToPoints(hill)
+hdf <- data.frame(hdf)
+
+
 # FIGURE 1 WILL BE A CONCEPTUAL DIAGRAM TYPE THING -----------------------------
 
 # FIGURE 2 NOVEL COMMUNITIES BY RCP --------------------------------------------
@@ -38,103 +47,141 @@ novel.20.rcp <- filter(dat, comm.type=="Novel", arbthresh == 0.2) %>%
   group_by(x, y, output_srtm, measure, RCP) %>%
   summarise(NoOfAnalogs = mean(value))
 
-ggplot(novel.20.rcp, aes(x, y, fill = NoOfAnalogs)) + 
+ggplot(NULL, aes(x, y)) + 
+  geom_raster(data = novel.20.rcp, aes(fill=NoOfAnalogs)) +
+  geom_raster(data = hdf, aes(alpha=layer)) +
   scale_fill_gradient(low = "blue", high = "white", name="Number of analog\ncommunities") +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0, 0.5), guide = "none") +
   facet_grid(measure ~ RCP) + 
   scale_x_continuous(name=expression(paste("Longitude (", degree, ")"))) + 
   scale_y_continuous(name=expression(paste("Latitude (", degree, ")"))) +
-  geom_raster() + coord_equal() + theme_classic(base_size=15) + 
+  coord_equal() + theme_classic(base_size=15) + 
   theme(strip.background = element_blank(), panel.margin = unit(2, "lines"))
 
-ggsave("Figures/Novel_by_RCP_20perc_thres.png", width = 17, height = 9)
+ggsave("Figures/Novel_by_RCP_20perc_thres.png", width = 9, height = 9)
 
 # 2b Analog ~ Elevation GAMs
 ggplot(novel.20.rcp, aes(x=output_srtm, y=round(NoOfAnalogs, 0))) + geom_point(alpha=0.01) + 
   facet_grid(measure~RCP) + 
   geom_smooth(method="gam",formula = y~s(x, k=20)) +
   labs(x="Elevation", y="Number of analog communities") +
-  theme_classic() + theme(strip.background=element_blank())
+  theme_classic(base_size=15) + theme(strip.background=element_blank())
 
-ggsave("Figures/Novel_elevation_gam_RCP_20perc.png", width = 17, height = 9)
+ggsave("Figures/Novel_elevation_gam_RCP_20perc.png", width = 9, height = 9)
 
 # FIGURE 3 DISAPPEARING COMMUNITIES BY RCP -------------------------------------
 diss.20.rcp <- filter(dat, comm.type=="Disappearing", arbthresh == 0.2) %>%
   group_by(x, y, output_srtm, measure, RCP) %>%
   summarise(NoOfAnalogs = mean(value))
 
-ggplot(diss.20.rcp, aes(x, y, fill = NoOfAnalogs)) + 
+ggplot(NULL, aes(x, y)) + 
+  geom_raster(data = diss.20.rcp, aes(fill=NoOfAnalogs)) +
+  geom_raster(data = hdf, aes(alpha=layer)) +
   scale_fill_gradient(low = "red", high = "white", name="Number of analog\ncommunities") +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0, 0.5), guide = "none") +
   facet_grid(measure ~ RCP) + 
-  labs(x="Longitude", y="Latitude") +
-  geom_raster() + coord_equal() + theme_classic(base_size=15) + 
+  scale_x_continuous(name=expression(paste("Longitude (", degree, ")"))) + 
+  scale_y_continuous(name=expression(paste("Latitude (", degree, ")"))) +
+  coord_equal() + theme_classic(base_size=15) + 
   theme(strip.background = element_blank(), panel.margin = unit(2, "lines"))
 
-ggsave("Figures/Disappearing_by_RCP_20perc_thres.png", width = 17, height = 9)
+ggsave("Figures/Disappearing_by_RCP_20perc_thres.png", width = 9, height = 9)
 
 # 3b Analog ~ Elevation GAMs
 ggplot(diss.20.rcp, aes(x=output_srtm, y=round(NoOfAnalogs, 0))) + geom_point(alpha=0.01) + 
   facet_grid(measure~RCP) + 
   geom_smooth(method="gam",formula = y~s(x, k=20), colour="red") +
   labs(x="Elevation", y="Number of analog communities") +
-  theme_classic() + theme(strip.background=element_blank())
+  theme_classic(base_size=15) + theme(strip.background=element_blank())
 
-ggsave("Figures/Diss_elevation_gam_RCP_20perc.png", width = 17, height = 9)
+ggsave("Figures/Diss_elevation_gam_RCP_20perc.png", width = 9, height = 9)
 
 # FIGURE 4 BOXPLOTS OF DIFFERENCES IN NUMBER OF ANALOGUES BETWEEN SCENARIOS-----
-input.dat <- filter(dat, arbthresh == 0.2) %>%
-  group_by(x, y, measure, comm.type, RCP) %>%
-  summarise(NoOfAnalogs = mean(value)) %>%
-  spread(RCP, NoOfAnalogs)
-
-input <- as.matrix(input.dat[,5:7])
-input.diff <- list(data.frame(var = "RCP2.6 - RCP4.5", value = input[,1] - input[,2]),
-                   data.frame(var = "RCP4.5 - RCP8.5", value = input[,2] - input[,3]),
-                   data.frame(var = "RCP2.6 - RCP8.5", value = input[,1] - input[,3]))
-input.diff <- do.call("rbind", input.diff)
-input.diff <- cbind(comm.type=input.dat$comm.type, measure = input.dat$measure, input.diff)
-input.diff$comm.type <- factor(input.diff$comm.type, levels=c("Novel", "Disappearing"))
-ggplot(input.diff, aes(x=var, y = value)) + 
-  geom_boxplot() + 
-  labs(x="", y="Difference in number of analog communities") +
-  geom_hline(colour="red") + 
-  facet_grid(measure~comm.type) + 
-  theme_classic() + 
-  theme(strip.background=element_blank())
-
-# for GCM
-input.dat <- filter(dat, arbthresh == 0.2) %>%
-  group_by(x, y, measure, comm.type, GCM) %>%
-  summarise(NoOfAnalogs = mean(value)) %>%
-  spread(GCM, NoOfAnalogs)
-
-ggplot(input.dat, aes(x=GCM, y=NoOfAnalogs)) + geom_boxplot() + 
-  facet_grid(measure~comm.type)
-input <- as.matrix(input.dat[,5:7])
-input.diff <- list(data.frame(var = "RCP2.6 - RCP4.5", value = input[,1] - input[,2]),
-                   data.frame(var = "RCP4.5 - RCP8.5", value = input[,2] - input[,3]),
-                   data.frame(var = "RCP2.6 - RCP8.5", value = input[,1] - input[,3]))
-input.diff <- do.call("rbind", input.diff)
-input.diff <- cbind(comm.type=input.dat$comm.type, measure = input.dat$measure, input.diff)
-input.diff$comm.type <- factor(input.diff$comm.type, levels=c("Novel", "Disappearing"))
-ggplot(input.diff, aes(x=var, y = value)) + 
-  geom_boxplot() + 
-  labs(x="", y="Difference in number of analog communities") +
-  geom_hline(colour="red") + 
-  facet_grid(measure~comm.type) + 
-  theme_classic() + 
-  theme(strip.background=element_blank())
-
-# friedman test for each group
-res <- list()
-for(com in unique(input.dat$comm.type)) {
-  for(mes in unique(input.dat$measure)) {
-    dat <- filter(input.dat, comm.type==com, measure==mes)
-    f <- friedman.test(as.matrix(dat[5:7]))
-    res[[paste0(com, mes)]] <- cbind(tidy(f), measure=mes, comm.type=com)
-  }
-}
-
-res <- do.call("rbind", res)
+# function to create all pairwise differences
+# pair.diff <- function(input){
+#   nm1 <- outer(colnames(input), colnames(input), paste, sep=" - ")
+#   indx1 <-  which(lower.tri(nm1, diag=TRUE))
+#   res <- outer(1:ncol(input), 1:ncol(input), 
+#                function(x,y) input[,x]-input[,y])
+#   colnames(res) <- nm1
+#   res <- res[-indx1]
+#   return(res)
+# }
+# 
+# input.dat <- filter(dat, arbthresh == 0.2) %>%
+#   group_by(x, y, measure, comm.type, RCP) %>%
+#   summarise(NoOfAnalogs = mean(value)) %>%
+#   spread(RCP, NoOfAnalogs)
+# 
+# input <- input.dat[,5:7]
+# input.diff <- pair.diff(input)
+# input.diff <- cbind(comm.type=input.dat$comm.type, measure = input.dat$measure, input.diff)
+# input.diff <- gather(input.diff, var, value, -comm.type, -measure)
+# input.diff$comm.type <- factor(input.diff$comm.type, levels=c("Novel", "Disappearing"))
+# input.diff$variable <- factor(input.diff$variable, levels=c("RCP2.6 - RCP4.5",
+#                                                             "RCP4.5 - RCP8.5",
+#                                                             "RCP2.6 - RCP8.5"))
+# ggplot(input.diff, aes(x=variable, y = value)) + 
+#   geom_boxplot() + 
+#   labs(x="", y="Difference in number of analog communities") +
+#   geom_hline(colour="red") + 
+#   facet_grid(measure~comm.type) + 
+#   theme_classic() + 
+#   theme(strip.background=element_blank())
+# 
+# # for GCM
+# input.dat <- filter(dat, arbthresh == 0.2) %>%
+#   group_by(x, y, measure, comm.type, GCM) %>%
+#   summarise(NoOfAnalogs = mean(value)) %>%
+#   spread(GCM, NoOfAnalogs)
+# 
+# input <- input.dat[,5:11]
+# input.diff <- pair.diff(input)
+# input.diff <- cbind(comm.type=input.dat$comm.type, measure = input.dat$measure, input.diff)
+# input.diff <- group_by(input.diff, comm.type, measure) %>%
+#   summarise_each(funs(mean(., na.rm=TRUE)))
+# 
+# 
+# input.diff <- gather(input.diff, var, value, -comm.type, -measure)
+# input.diff$comm.type <- factor(input.diff$comm.type, levels=c("Novel", "Disappearing"))
+# 
+# ggplot(input.diff, aes(x=variable, y = value)) + 
+#   geom_boxplot() + 
+#   labs(x="", y="Difference in number of analog communities") +
+#   geom_hline(colour="red") + 
+#   facet_grid(measure~comm.type) + 
+#   theme_classic() + 
+#   theme(strip.background=element_blank()) + 
+#   theme(axis.text.x=element_text(angle=-90))
+# 
+# input <- as.matrix(input.dat[,5:11])
+# input.diff <- list(data.frame(var = "RCP2.6 - RCP4.5", value = input[,1] - input[,2]),
+#                    data.frame(var = "RCP4.5 - RCP8.5", value = input[,2] - input[,3]),
+#                    data.frame(var = "RCP2.6 - RCP8.5", value = input[,1] - input[,3]))
+# input.diff <- do.call("rbind", input.diff)
+# input.diff <- cbind(comm.type=input.dat$comm.type, measure = input.dat$measure, input.diff)
+# input.diff$comm.type <- factor(input.diff$comm.type, levels=c("Novel", "Disappearing"))
+# ggplot(input.diff, aes(x=var, y = value)) + 
+#   geom_boxplot() + 
+#   labs(x="", y="Difference in number of analog communities") +
+#   geom_hline(colour="red") + 
+#   facet_grid(measure~comm.type) + 
+#   theme_classic() + 
+#   theme(strip.background=element_blank())
+# 
+# # friedman test for each group
+# res <- list()
+# for(com in unique(input.dat$comm.type)) {
+#   for(mes in unique(input.dat$measure)) {
+#     dat <- filter(input.dat, comm.type==com, measure==mes)
+#     f <- friedman.test(as.matrix(dat[5:11]))
+#     res[[paste0(com, mes)]] <- cbind(tidy(f), measure=mes, comm.type=com)
+#   }
+# }
+# 
+# res <- do.call("rbind", res)
 
 
 
@@ -143,11 +190,16 @@ thresh_sa.novel <- filter(dat, comm.type=="Novel") %>%
   group_by(x, y, measure, arbthresh) %>%
   summarise(NoOfAnalogs = mean(value))
 
-ggplot(thresh_sa.novel, aes(x, y, fill = NoOfAnalogs)) + 
+ggplot(NULL, aes(x, y)) + 
+  geom_raster(data = thresh_sa.novel, aes(fill=NoOfAnalogs)) +
+  geom_raster(data = hdf, aes(alpha=layer)) +
   scale_fill_gradient(low = "blue", high = "white", name="Number of analog\ncommunities") +
-  labs(x="Longitude", y="Latitude") +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0, 0.5), guide = "none") +
   facet_grid(measure ~ arbthresh) + 
-  geom_raster() + coord_equal() + theme_classic(base_size=15) + 
+  scale_x_continuous(name=expression(paste("Longitude (", degree, ")"))) + 
+  scale_y_continuous(name=expression(paste("Latitude (", degree, ")"))) +
+  coord_equal() + theme_classic(base_size=15) + 
   theme(strip.background = element_blank(), panel.margin = unit(2, "lines"))
 
 ggsave("Figures/Threshold_SA_Novel.png", width = 17, height = 9)
@@ -157,12 +209,18 @@ thresh_sa.diss <- filter(dat, comm.type=="Disappearing") %>%
   group_by(x, y, measure, arbthresh) %>%
   summarise(NoOfAnalogs = mean(value))
 
-ggplot(thresh_sa.diss, aes(x, y, fill = NoOfAnalogs)) + 
+ggplot(NULL, aes(x, y)) + 
+  geom_raster(data = thresh_sa.diss, aes(fill=NoOfAnalogs)) +
+  geom_raster(data = hdf, aes(alpha=layer)) +
   scale_fill_gradient(low = "red", high = "white", name="Number of analog\ncommunities") +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0, 0.5), guide = "none") +
   facet_grid(measure ~ arbthresh) + 
-  labs(x="Longitude", y="Latitude") +
-  geom_raster() + coord_equal() + theme_classic(base_size=15) + 
+  scale_x_continuous(name=expression(paste("Longitude (", degree, ")"))) + 
+  scale_y_continuous(name=expression(paste("Latitude (", degree, ")"))) +
+  coord_equal() + theme_classic(base_size=15) + 
   theme(strip.background = element_blank(), panel.margin = unit(2, "lines"))
+
 
 ggsave("Figures/Threshold_SA_Disappearing.png", width = 17, height = 9)
 
@@ -171,11 +229,16 @@ novel.20.gcm <- filter(dat, comm.type=="Novel", arbthresh == 0.2) %>%
   group_by(x, y, measure, GCM) %>%
   summarise(NoOfAnalogs = mean(value))
 
-ggplot(novel.20.gcm, aes(x, y, fill = NoOfAnalogs)) + 
+ggplot(NULL, aes(x, y)) + 
+  geom_raster(data = novel.20.gcm, aes(fill=NoOfAnalogs)) +
+  geom_raster(data = hdf, aes(alpha=layer)) +
   scale_fill_gradient(low = "blue", high = "white", name="Number of analog\ncommunities") +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0, 0.5), guide = "none") +
   facet_grid(measure ~ GCM) + 
-  labs(x="Longitude", y="Latitude") +
-  geom_raster() + coord_equal() + theme_classic(base_size=15) + 
+  scale_x_continuous(name=expression(paste("Longitude (", degree, ")"))) + 
+  scale_y_continuous(name=expression(paste("Latitude (", degree, ")"))) +
+  coord_equal() + theme_classic(base_size=15) + 
   theme(strip.background = element_blank(), panel.margin = unit(2, "lines"))
 
 ggsave("Figures/Novel_by_GCM_20perc_thres.png", width = 18, height = 9)
@@ -185,11 +248,16 @@ diss.20.gcm <- filter(dat, comm.type=="Disappearing", arbthresh == 0.2) %>%
   group_by(x, y, measure, GCM) %>%
   summarise(NoOfAnalogs = mean(value))
 
-ggplot(diss.20.gcm, aes(x, y, fill = NoOfAnalogs)) + 
+ggplot(NULL, aes(x, y)) + 
+  geom_raster(data = diss.20.gcm, aes(fill=NoOfAnalogs)) +
+  geom_raster(data = hdf, aes(alpha=layer)) +
   scale_fill_gradient(low = "red", high = "white", name="Number of analog\ncommunities") +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0, 0.5), guide = "none") +
   facet_grid(measure ~ GCM) + 
-  labs(x="Longitude", y="Latitude") +
-  geom_raster() + coord_equal() + theme_classic(base_size=15) + 
+  scale_x_continuous(name=expression(paste("Longitude (", degree, ")"))) + 
+  scale_y_continuous(name=expression(paste("Latitude (", degree, ")"))) +
+  coord_equal() + theme_classic(base_size=15) + 
   theme(strip.background = element_blank(), panel.margin = unit(2, "lines"))
 
 ggsave("Figures/Disappearing_by_GCM_20perc_thres.png", width = 18, height = 9)
